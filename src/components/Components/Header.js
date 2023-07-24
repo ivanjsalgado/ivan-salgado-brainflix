5e61bf743063965c6496472eeb65ca31c2642025
Ivan Salgado <ivanjsalgado1@gmail.com>
2023-07-24 17:11:38 -0400

    Removed default styling and brought in logo

---
+++ src/components/Components/Header.js
@@ -0,0 +1,16 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+      Header
+    </header>
+  );
+}
+
+export default Header;
