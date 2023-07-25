b4b9091f19e264ff9725eae02854dc6b259c08e7
Ivan Salgado <ivanjsalgado1@gmail.com>
2023-07-24 22:03:27 -0400

    Created all partials, added the fonts for the project and created header elements

--- src/components/Components/Header.js
+++ src/components/Components/Header.js
@@ -1,14 +1,27 @@
 import "./Header.scss";
+import Logo from "../../assets/images/BrainFlix-logo.svg";
+import ArtistImage from "../../assets/images/Mohan-muruge.jpg";
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li></li>
+    <header className="header">
+      <nav className="header__nav">
+        <ul className="header__links">
+          <li className="header__link">
+            <img className="header__logo" src={Logo} alt="Logo" />
+          </li>
         </ul>
       </nav>
-      Header
+      <div className="header__container">
+        <form className="header__form">
+          <input
+            className="header__search"
+            type="search"
+            placeholder="Search"
+          />
+        </form>
+        <img className="header__avatar" src={ArtistImage} alt="Artist Avatar" />
+      </div>
     </header>
   );
 }
