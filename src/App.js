02a3087815db682948bbbeabb71dd4fe930110c9
Ivan Salgado <ivanjsalgado1@gmail.com>
2023-07-25 15:13:45 -0400

    Initialized hero section and placed a dummy video image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Components/Header";
+import Hero from "./components/Components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
