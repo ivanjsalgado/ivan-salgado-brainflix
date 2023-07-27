59c76e55ad188f4485bb03e2fb156a0747221f21
Ivan Salgado <101019855+ivanjsalgado@users.noreply.github.com>
2023-07-27 16:36:27 -0400

    Updated desktop styling and Added Video Section
    - Both Video and Videos components were created and added
    - Implemented desktop styling for CommentsForm, CommentsList, Video and Videos
    - Added divs within App so I can accomplish the appearance for the desktop version

--- src/components/Components/Hero.js
+++ src/components/Components/Hero.js
@@ -2,13 +2,13 @@ import "./Hero.scss";
 
 function Hero() {
   return (
-    <div className="hero">
+    <article className="hero">
       <video
         className="hero__video"
         controls
         poster="https://i.imgur.com/l2Xfgpl.jpg"
       ></video>
-    </div>
+    </article>
   );
 }
 
---
+++ src/components/Components/Videos.js
@@ -0,0 +1,14 @@
+import "./Videos.scss";
+import Video from "./Video";
+
+function Videos() {
+  return (
+    <section className="videos">
+      <h2 className="videos__heading">NEXT VIDEOS</h2>
+      <Video />
+      <Video />
+    </section>
+  );
+}
+
+export default Videos;
